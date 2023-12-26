13e9f1bf569b1bb6ab82083c512488d653f8b66d
Ferhat <ucarferhat3@gmail.com>
2023-12-26 15:40:50 +0100

    fix typo file names

--- app/(main)/(routes)/servers/[serverId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/page.tsx
@@ -1,15 +1,50 @@
-import React from "react";
-import { NavigationSidebar } from "@/components/navigation/navigation-sidebar";
+import { redirectToSignIn } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
 
-const ServerIdPage = () => {
-  return (
-    <div className="h-full flex">
-      <div className="hidden md:flex h-full w-[72px] z-30 flex-col fixed inset-y-0 flex-shrink-0">
-        <NavigationSidebar />
-      </div>
-      <div className="md:pl-[72px] h-full">Server ID Page</div>
-    </div>
-  );
+import { currentProfile } from "@/lib/current-profile";
+import { db } from "@/lib/db";
+
+interface ServerIdPageProps {
+  params: {
+    serverId: string;
+  };
+}
+
+const ServerIdPage = async ({ params }: ServerIdPageProps) => {
+  const profile = await currentProfile();
+
+  if (!profile) {
+    return redirectToSignIn();
+  }
+
+  const server = await db.server.findUnique({
+    where: {
+      id: params.serverId,
+      members: {
+        some: {
+          profileId: profile.id,
+        },
+      },
+    },
+    include: {
+      channels: {
+        where: {
+          name: "general",
+        },
+        orderBy: {
+          createdAt: "asc",
+        },
+      },
+    },
+  });
+
+  const initialChannel = server?.channels[0];
+
+  if (initialChannel?.name !== "general") {
+    return null;
+  }
+
+  return redirect(`/servers/${params.serverId}/channels/${initialChannel?.id}`);
 };
 
 export default ServerIdPage;
